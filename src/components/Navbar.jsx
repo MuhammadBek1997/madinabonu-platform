2ef3428335b5a70913fc423e0a326840f511c074
SarvinozGafurova <sarvinozgafurova008@gmail.com>
2025-12-27 18:14:36 +0500

    Sidebar navbar set

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,12 @@ import '../styles/Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <div className="word-logo">
+      <div className="nav-logo">
+        <img src="/images/saveb.png" alt="logo"  id='logotip'/>
+      </div>
      Madinabonu
+    </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
-import { Save } from 'lucide-react'
-import  { useState } from 'react'
+import '../styles/Home.css'
+
 
 const Home = () => {
-  
+
   return (
-    <div>
-       
-    </div>
+    <section>
+      <div className='hero'>
+        <div className="for-words-btns">
+          <h1>
+            Madinabonu - online kurslari
+          </h1>
+          <p>
+            Madinabonu — bu sizga fanlarni o‘rganishda yordam beradigan platforma.
+            Pullik darslardagi ma’lumotlarni bepul o‘rganing. Barchasi bir joyda!
+          </p>
+          <div className="courses-btn">
+          <div className="btn">Kurslar</div>
+          <div className="btn">Loyihalar</div>
+          </div>
+        </div>
+        <div className="nav-logo">
+        <img src="/images/logotip.png" alt="logo" className='img-hero' />
+      </div>
+      </div>
+    </section>
   )
 }
 
