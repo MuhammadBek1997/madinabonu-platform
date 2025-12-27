bda3c3620b5a04a752b2960f25cbc8de610af5c3
SarvinozGafurova <sarvinozgafurova008@gmail.com>
2025-12-27 16:31:09 +0500

    yangilik kiritildi

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,15 @@ function App() {
   return (
     <>
       <Sidebar/>
-      <Routes>
+      <div>
+        <Navbar/>
+        <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/about' element={ <About/> }/>
         <Route path='/contact' element={ <Contact/> }/>
       </Routes>
+      </div>
+      
     </>
   )
 }
