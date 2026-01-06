2d278b42fc4ed561a95ca2404574ad8ef1a425ad
SarvinozGafurova <sarvinozgafurova008@gmail.com>
2026-01-06 17:54:01 +0500

    o'zgarish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,31 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="word-logo">
-      <div className="nav-logo">
-        <img src="/images/saveb.png" alt="logo"  id='logotip'/>
+        <div className="nav-logo">
+          <img src="/images/saveb.png" alt="logo" id='logotip' />
+        </div>
+        Madinabonu
       </div>
-     Madinabonu
-    </div>
+      <div className='nav-btns'>
+        {/* theme toggle */}
+        <button onClick={() => setOpenTheme(!openTheme)}>
+          {theme == "dark" ? '🌙 Dark' : '☀️Light'}
+        </button>
+
+        {/* theme menu */}
+        <div className='theme-menu' style={{
+          display: openTheme ? "flex" : "none"
+        }}>
+          <button onClick={() => handleChangeTheme("dark")}>
+            🌙 Dark
+          </button>
+          <button onClick={() => handleChangeTheme("light")}>
+            ☀️Light
+          </button>
+        </div>
+        {/* end of theme menu */}
+
+      </div>
     </div>
   )
 }
