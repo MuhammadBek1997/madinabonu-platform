b103e90fbfd0a453aa2c15ccc628b32dc551259a
xamroyevzafar220-sys <xamroyevzafar220@gmail.com>
2026-01-07 15:02:40 +0500

    comment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,20 @@ import '../styles/Home.css'
 
 const Home = () => {
 
+  let homecourse = [
+    {
+      img: './images/matem.png',
+      desc: 'Math group'
+    },
+    {
+      img: './images/matem.png',
+      desc: 'Math group'
+    },
+    {
+      img: './images/ing.png',
+      desc: 'English group'
+    }
+  ]
   return (
     <section>
       <div className='hero'>
@@ -15,14 +29,104 @@ const Home = () => {
             Pullik darslardagi ma’lumotlarni bepul o‘rganing. Barchasi bir joyda!
           </p>
           <div className="courses-btn">
-          <div className="btn">Kurslar</div>
-          <div className="btn">Loyihalar</div>
+            <div className="btn">Kurslar</div>
+            <div className="btn">Loyihalar</div>
           </div>
         </div>
         <div className="nav-logo">
-        <img src="/images/logotip.png" alt="logo" className='img-hero' />
+          <img src="/images/logotip.png" alt="logo" className='img-hero' />
+        </div>
       </div>
-      </div>
+
+
+
+
+{/* kurslar haqida */}
+      <main>
+        <div className='kurs'>
+          <h1>
+            Kurslar
+          </h1>
+          <img src="images/icons8-chevron-right-30.png" alt="chevron-right" />
+        </div>
+
+        <div id='item-list'>
+          {
+            homecourse.map((item, index) => {
+              return (
+                <div className='card' key={index}>
+                  <div className='card-cont'>
+                    <img src={item.img} alt="" />
+                    <h2>
+                      {item.desc}
+                    </h2>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+
+        </div>
+      </main>
+       {/* ustozlar haqida */}
+      <main>
+       
+        <div className='kurs'>
+          <h1>
+            Teacher
+          </h1>
+          <img src="images/icons8-chevron-right-30.png" alt="chevron-right" />
+        </div>
+
+        <div id='item-list'>
+          {
+            homecourse.map((item, index) => {
+              return (
+                <div className='card' key={index}>
+                  <div className='card-cont'>
+                    <img src={item.img} alt="" />
+                    <h2>
+                      {item.desc}
+                    </h2>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+
+        </div>
+      </main>
+      
+{/* oquvchilar  haqida */}
+      <main>
+        <div className='kurs'>
+          <h1>
+            Top Students
+          </h1>
+          <img src="images/icons8-chevron-right-30.png" alt="chevron-right" />
+        </div>
+
+        <div id='item-list'>
+          {
+            homecourse.map((item, index) => {
+              return (
+                <div className='card' key={index}>
+                  <div className='card-cont'>
+                    <img src={item.img} alt="" />
+                    <h2>
+                      {item.desc}
+                    </h2>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+
+        </div>
+      </main>
     </section>
   )
 }
