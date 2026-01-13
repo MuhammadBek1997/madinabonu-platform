cad8c5e5b7162aa746a754169efb1c951e6690f1
SarvinozGafurova <sarvinozgafurova008@gmail.com>
2026-01-13 17:55:30 +0500

    o'zgarish

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -3,15 +3,15 @@ import '../styles/Teachers.css'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 const Teachers = ({ courses }) => {
-    const [cardToggle, setCardToggle] = useState(false)
+    const [cardToggle, setCardToggle] = useState(null)
     return (
 
         <section className='course-list'>
             {
                 courses.map((item, index) => {
                     return (
-                        <div className='courses-card' >
-                            <div className={`course-card ${cardToggle ? "card-open" : ""}`} key={index}>
+                        <div className='course-card' >
+                            <div className={`course-card-cont ${cardToggle ? "card-open" : ""}`} key={index}>
                                 <img className='pictures' src={item.img} alt={item.name} />
                                 <div className="title-and-btn">
                                     <div className="words">
@@ -27,13 +27,7 @@ const Teachers = ({ courses }) => {
                                         {!cardToggle ? <ChevronRight /> : <ChevronLeft />}
                                     </button>
                                 </div>
-                                <div className="hover-state">
-                                {/* {sToggle ? "":"  "}🏠 */}
-                                   
-                                    <button>
-
-                                    </button>
-                                </div>
+                               
                             </div>
                         </div>
                     )
