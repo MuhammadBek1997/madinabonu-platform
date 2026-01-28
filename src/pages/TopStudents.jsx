961e6ecb4cdbaf61a08ca54240a9bfe6bce4a990
SarvinozGafurova <sarvinozgafurova008@gmail.com>
2026-01-28 14:27:27 +0500

    cardlar o'zagrtirildi

--- src/pages/TopStudents.jsx
+++ src/pages/TopStudents.jsx
@@ -1,21 +1,21 @@
-import  { useState } from 'react'
+import { useState } from 'react'
 import '../styles/Students.css'
 import StudentCard from '../components/StudentCard'
 import { useGlobalContext } from '../Context'
 
 const TopStudents = () => {
 
-  let {courses, setsToggle,sToggle } = useGlobalContext()
-  
+  let { courses, setsToggle, sToggle } = useGlobalContext()
+
   return (
     <section className='course-list'>
-    {
+      {
         courses.map((item, index) => {
-            return <StudentCard key={index} {...item} />
+          return <StudentCard key={index} {...item} />
         })
-    }
-</section>
-   
+      }
+    </section>
+
   )
 }
 
