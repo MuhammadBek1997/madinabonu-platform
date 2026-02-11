746e26e751078536cec1cef84a83534e41d37f37
SarvinozGafurova <sarvinozgafurova008@gmail.com>
2026-02-11 17:31:42 +0500

    yangilandi

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -4,18 +4,35 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useGlobalContext } from '../Context'
 
-const StudentCard = ({achievements,GroupPhotos,WorkExperience,students,AboutTeacher}) => {
+const StudentCard = ({ achievements, GroupPhotos, WorkExperience, students, AboutTeacher, img, name }) => {
 
-    let{ courses } = useGlobalContext()
+    let { courses } = useGlobalContext()
 
     const [cardButton, setCardButton] = useState(false)
 
-    return (
-        <div>
+    return (<tr>
+ 
+        <td>  <img src={img} alt="" className='avatars' /> </td>
+        <td>
+            <div class="student-info">
+                Sophia Wilson
+            </div>
+        </td>
+        <td>522bcs009</td>
+        <td>{name}</td>
+        <td> <button className='student-single'>Batafsil</button></td>
 
-          
 
-        </div>
+
+
+
+
+
+        {/* </div> */}
+
+
+        {/* </div> */}
+    </tr>
     )
 }
 
--- src/pages/TopStudents.jsx
+++ src/pages/TopStudents.jsx
@@ -8,13 +8,35 @@ const TopStudents = () => {
   let { courses, setsToggle, sToggle } = useGlobalContext()
 
   return (
-    <section className='course-list'>
-      {
-        courses.map((item, index) => {
-          return <StudentCard key={index} {...item} />
-        })
-      }
-    </section>
+    <div className='students'>
+      <div className="students-cont">
+      <table className="students-table">
+        <thead className='head-part'>
+          <tr>
+            <th>Avatar</th>
+            <th>Familiya</th>
+            <th>Ism</th>
+            <th>O'qituvchi</th>
+            <th>Batafsil</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            courses.map((item, index) => {
+              return <StudentCard key={index} {...item} />
+            })
+          }
+          <div class="pagination">
+            <div>Previous</div>
+            <div>Page 1 of 12</div>
+            <div>Next</div>
+          </div>
+
+        </tbody>
+      </table>
+      </div>
+    </div>
 
   )
 }
